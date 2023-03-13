ca296266c860431f58d6b7adba4d6179f2c68518
harish8802 <96900707+harish8802@users.noreply.github.com>
2023-03-13 16:03:18 +0530

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -1,36 +1,17 @@
 $(document).ready(function() {
-  $.ajax({
-      url: 'get_profile.php',
-      type: 'GET',
-      dataType: 'json',
-      success: function(data) {
-          $('#name').val(data.name);
-          $('#email').val(data.email);
-          $('#age').val(data.age);
-          $('#dob').val(data.dob);
-          $('#contact').val(data.contact);
-      },
-      error: function(jqXHR, textStatus, errorThrown) {
-          console.log('Error retrieving profile data: ' + textStatus + ' - ' + errorThrown);
-      }
-  });
-
-
-  $('#profile-form').submit(function(e) {
-      e.preventDefault();
-      var formData = $(this).serialize();
-
-      $.ajax({
-          url: 'update_profile.php',
-          type: 'POST',
-          data: formData,
-          dataType: 'json',
-          success: function(data) {
-              alert('Profile updated successfully');
-          },
-          error: function(jqXHR, textStatus, errorThrown) {
-              console.log('Error updating profile: ' + textStatus + ' - ' + errorThrown);
-          }
-      });
-  });
+    $('#profileForm').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: 'POST',
+            url: 'php/profile.php',
+            data: $(this).serialize(),
+            success: function(response) {
+                if (response == 'success') {
+                    alert("Profile updated successfully");
+                } else {
+                    alert(response);
+                }
+            }
+        });
+    });
 });
