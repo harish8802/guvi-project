520e1f7e754ca27d549f91a54e6826bbcf3f48f8
harish8802 <96900707+harish8802@users.noreply.github.com>
2023-03-13 16:02:46 +0530

    Update profile.php

--- php/profile.php
+++ php/profile.php
@@ -1,24 +1,30 @@
 <?php
 require 'vendor/autoload.php';
-
-$client = new MongoDB\Client("mongodb://localhost:27017");
-
+$host = "mongodb://localhost:27017";
+$dbname = "testdb";
 $collection = $client->test->users;
+$mongodb = new MongoDB\Driver\Manager($host);
 
-$username = $_POST['username'];
+if (!$mongodb) {
+    die("Connection failed");
+}
+
+$mongodb = new MongoDB\Driver\Manager("mongodb://localhost:27017");
+
 $age = $_POST['age'];
 $dob = $_POST['dob'];
 $contact = $_POST['contact'];
 
+$bulk = new MongoDB\Driver\BulkWrite();
+$bulk->update(
+    ['_id' => new MongoDB\BSON\ObjectID($_COOKIE['userId'])],
+    ['$set' => [
+        'age' => $age,
+        'dob' => $dob,
+        'contact' => $contact
+    ]]
+);
+$mongodb->executeBulkWrite('mydatabase.users', $bulk);
 
-$user = [
-'username' => $username,
-'age' => $age,
-'dob' => $dob,
-'contact' => $contact
-];
-
-$result = $collection->insertOne($user);
-
-echo json_encode(['success' => true]);
+echo "success";
 ?>
