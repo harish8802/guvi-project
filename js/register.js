3b958ce6bd625547a0b65829d6d8db3c99fbed60
harish8802 <96900707+harish8802@users.noreply.github.com>
2023-03-13 15:49:49 +0530

    Update register.js

--- js/register.js
+++ js/register.js
@@ -1,17 +1,14 @@
 $(document).ready(function() {
-  // Get the profile data from MongoDB
   $.ajax({
     url: 'get_profile.php',
     method: 'GET',
     success: function(data) {
-      // Populate the form fields with the retrieved data
       $('#name').val(data.name);
       $('#email').val(data.email);
       $('#age').val(data.age);
     }
   });
 
-  // Submit the form using Ajax
   $('#profile-form').submit(function(event) {
     event.preventDefault();
     var formData = $(this).serialize();
@@ -20,7 +17,6 @@ $(document).ready(function() {
       method: 'POST',
       data: formData,
       success: function(response) {
-        // Show a success message
         $('#success-message').text(response);
       }
     });
