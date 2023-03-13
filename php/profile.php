aedf492289367461c87302dcb5e31fb18c78f83a
harish8802 <96900707+harish8802@users.noreply.github.com>
2023-03-13 15:46:27 +0530

    Update profile.php

--- php/profile.php
+++ php/profile.php
@@ -1,20 +1,16 @@
 <?php
-// Include the MongoDB library
 require 'vendor/autoload.php';
 
-// Create a new MongoDB client
 $client = new MongoDB\Client("mongodb://localhost:27017");
 
-// Select the database and collection
 $collection = $client->test->users;
 
-// Get the form data
 $username = $_POST['username'];
 $age = $_POST['age'];
 $dob = $_POST['dob'];
 $contact = $_POST['contact'];
 
-// Create a new user document
+
 $user = [
 'username' => $username,
 'age' => $age,
@@ -22,9 +18,7 @@ $user = [
 'contact' => $contact
 ];
 
-// Insert the user document into the collection
 $result = $collection->insertOne($user);
 
-// Return the result as JSON
 echo json_encode(['success' => true]);
 ?>
